69f7d3088d22b0a6322e941727cd92e05e5286d9
Ongeziwe <ongeziwejunior6@gmail.com>
2024-10-18 08:53:34 +0200

    Removing all deleted files

--- src/main/java/za/ac/cput/service/ContactService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.service;
-
-import za.ac.cput.domain.Contact;
-
-import java.util.List;
-
-public interface ContactService extends IService<Contact, String>{
-    List<Contact> getAll();
-}
